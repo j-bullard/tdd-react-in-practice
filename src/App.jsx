8b696bfa9ebde0783ab35972d5add0b9354830e4
Jason Bullard <jason@bullard.io>
2024-07-10 18:04:28 -0500

    add new recipe form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+
 export default function App() {
+  const [recipeFormShown, setRecipeFormShown] = useState(false);
+
+  const handleSubmitRecipe = (e) => {};
+
   return (
     <div className="app">
       <h1 className="app-header">My Recipes</h1>
       <p>There are no recipes to list.</p>
+      {recipeFormShown ? (
+        <form id="recipe-form" name="recipe-form" onSubmit={handleSubmitRecipe}>
+          <div>
+            <label htmlFor="recipeName">Recipe Name:</label>
+            <input type="text" name="recipeName" id="recipeName" />
+          </div>
+
+          <div>
+            <label htmlFor="recipeInstructions">Recipe Instructions: </label>
+            <textarea
+              name="recipeInstructions"
+              id="recipeInstructions"
+            ></textarea>
+          </div>
+        </form>
+      ) : (
+        <button onClick={() => setRecipeFormShown(true)}>Add Recipe</button>
+      )}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 test("As a Chef, I want to store my recipes so that I can keep track of them.", () => {
@@ -16,4 +17,38 @@ test("As a Chef, I want to store my recipes so that I can keep track of them.", 
 
   // 5. Recipe list should be below recipe header
   expect(recipeHeader.compareDocumentPosition(recipeList)).toBe(4);
+});
+
+test("contains an add recipe button", async () => {
+  // 1. render the landing page
+  render(<App />);
+
+  // 2. wait for the page to load (implied)
+  // 3. should see a button on the page that says "Add Recipe"
+  let button = screen.getByRole("button", { name: "Add Recipe" });
+  expect(button).toBeInTheDocument();
+
+  // 4. the Add Recipe button should be below the heading that reads "My Recipes"
+  let recipeHeader = screen.getByText("My Recipes");
+  expect(recipeHeader.compareDocumentPosition(button)).toBe(4);
+
+  // Click the button
+  userEvent.click(button);
+
+  // wait for the form to appear on the screen (override the default of 1000ms as an example)
+  let form = await screen.findByRole("form", undefined, { timeout: 3000 });
+  expect(form).toBeInTheDocument();
+
+  // then i should see a form with fields: "Recipe Name" and "Recipe Instructions"
+  expect(
+    screen.getByRole("textbox", { name: /Recipe Name/i })
+  ).toBeInTheDocument();
+  expect(
+    screen.getByRole("textbox", { name: /Recipe Instructions/i })
+  ).toBeInTheDocument();
+
+  // and the "Add Recipe" button should no longer be on the screen
+  // use queryBy instead of getBy because getBy throws an error when it doesn't have exactly 1 match
+  button = screen.queryByRole("button", { name: "Add Recipe" });
+  expect(button).toBeNull();
 });
