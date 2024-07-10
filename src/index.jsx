5904202049c6176f89a37c7327acef128ad45b10
Jason Bullard <jason@bullard.io>
2024-07-10 17:00:02 -0500

    entry point

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import { Fragment } from "react";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <Fragment>
+    <></>
+  </Fragment>
+);
